41eb2f91bbbadc6734eeab8ede456b4be3cdbf9d
carlosLapa <carlosferreiralapa@gmail.com>
2023-11-17 16:23:58 +0000

    ScoreControllerRA tests all green

--- src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
@@ -1,16 +1,26 @@
 package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
 import org.json.JSONException;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.baseURI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class ScoreControllerRA {
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+class ScoreControllerRA {
 
     private String clientUsername, clientPassword, adminUsername, adminPassword;
     private String adminToken, clientToken, invalidToken;
+
+    private Map<String, Object> saveMovieScore;
 
     @BeforeEach
     public void setup() throws JSONException {
@@ -25,19 +35,57 @@ public class ScoreControllerRA {
         adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
         invalidToken = adminToken + "xpto";
 
+        saveMovieScore = new HashMap<>();
+        saveMovieScore.put("movieId", 1000L);
+        saveMovieScore.put("score", 4);
 
     }
 
-    // Implementar Map
     @Test
-    public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {
+    void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {
+
+        JSONObject newMovieScore = new JSONObject(saveMovieScore);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(newMovieScore)
+                .put("/scores")
+                .then()
+                .statusCode(404);
     }
 
     @Test
-    public void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() throws Exception {
+    void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() throws Exception {
+        saveMovieScore.put("movieId", null);
+        JSONObject newMovieScore = new JSONObject(saveMovieScore);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(newMovieScore)
+                .put("/scores")
+                .then()
+                .statusCode(422);
     }
 
     @Test
-    public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {
+    void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {
+        saveMovieScore.put("score", -1.1);
+        JSONObject movieScore = new JSONObject(saveMovieScore);
+
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(movieScore)
+                .put("/scores")
+                .then()
+                .statusCode(422);
     }
 }
